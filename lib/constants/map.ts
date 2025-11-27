1d0256f0901efb9381f75aabb032bf4a39638a08
Vj Mabansag <mabansagbj@gmail.com>
2025-11-27 23:34:18 +0800

    chore(map): expand zoom range and wire min/max zoom

--- lib/constants/map.ts
+++ lib/constants/map.ts
@@ -2,8 +2,8 @@ import type { LatLng } from "@/lib/types";
 
 export const MAP_DEFAULT_CENTER: LatLng = { lat: 10.74450, lng: 124.79194 }; // Approx VSU campus;
 export const MAP_DEFAULT_ZOOM = 16;
-export const MAP_MIN_ZOOM = 15;
-export const MAP_MAX_ZOOM = 19;
+export const MAP_MIN_ZOOM = 14;
+export const MAP_MAX_ZOOM = 22;
 
 export const MAP_TILES = {
   url: "https://cartodb-basemaps-{s}.global.ssl.fastly.net/light_nolabels/{z}/{x}/{y}.png",
