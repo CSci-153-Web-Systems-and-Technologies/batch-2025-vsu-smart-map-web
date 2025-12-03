f5238e5304c0b54b680229705dfb14d69de1e219
Vj Mabansag <mabansagbj@gmail.com>
2025-12-04 01:41:23 +0800

    feat(validation): add facility form schema with discriminated union

--- lib/constants/forms.ts
+++ lib/constants/forms.ts
@@ -5,6 +5,7 @@ export const VALIDATION_LIMITS = {
     description: { max: 400 },
   },
   facility: {
+    code: { min: 1, max: 16 },
     name: { min: 2, max: 120 },
     description: { max: 400 },
   },
