1fca0080a73c697be40b972aab79764c967aa5fc
Vj Mabansag <mabansagbj@gmail.com>
2025-12-07 10:02:00 +0800

    chore(directory): remove unused category filters component

--- components/directory/index.ts
+++ components/directory/index.ts
@@ -1,10 +1,7 @@
 export { FacilityCard, type FacilityCardProps } from "./facility-card";
 export { DirectoryList, type DirectoryListProps } from "./directory-list";
 export { DirectorySearch, type DirectorySearchProps } from "./directory-search";
-export {
-  DirectoryCategoryFilters,
-  type DirectoryCategoryFiltersProps,
-} from "./directory-category-filters";
+
 export {
   DirectoryContainer,
   type DirectoryContainerProps,
