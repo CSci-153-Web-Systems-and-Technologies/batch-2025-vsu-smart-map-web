459f0074d0197234b96d99e09cd6e2f35e4b1452
Vj Mabansag <mabansagbj@gmail.com>
2025-12-08 14:31:29 +0800

    feat(map): add satellite tile URL to map constants

--- lib/constants/map.ts
+++ lib/constants/map.ts
@@ -8,7 +8,8 @@ export const MAP_MAX_ZOOM = 20;
 export const MAP_TILES = {
   url: "https://{s}.basemaps.cartocdn.com/light_nolabels/{z}/{x}/{y}.png",
   darkUrl: "https://{s}.basemaps.cartocdn.com/dark_nolabels/{z}/{x}/{y}.png",
+  satelliteUrl: "https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}",
   attribution:
-    '© <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors · © <a href="https://carto.com/attributions">CARTO</a>',
+    '© <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors · © <a href="https://carto.com/attributions">CARTO</a> · Tiles © Esri',
   maxNativeZoom: 18,
 };
