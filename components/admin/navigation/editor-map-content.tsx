ed1e6ec00b2519b128abbfa75353522ad693a983
Vj <mabansagbj@gmail.com>
2026-01-31 12:58:25 +0000

    fix(map): Import leaflet CSS in editor map

--- components/admin/navigation/editor-map-content.tsx
+++ components/admin/navigation/editor-map-content.tsx
@@ -1,5 +1,7 @@
 "use client";
 
+import "leaflet/dist/leaflet.css";
+
 import { useEffect, useState, useCallback } from "react";
 import { useTheme } from "next-themes";
 import { MapContainer, TileLayer, CircleMarker, Polyline, useMapEvents, Marker, ZoomControl } from "react-leaflet";
